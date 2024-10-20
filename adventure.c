fb548cc601a9700032641ab8e45df9d3a5b64089
BIIGORANGE <bigorangecreations@gmail.com>
2024-10-20 12:47:09 -0700

    movement system, quit game
    -created movement system to move in 2d array
    -allowed user to quit game by giving char input
    
    NEXT TIME:
    -Figure out how to set limit or "walls" in array. going to far 'e' causes the array to move 's'. I don't want that, I want there to be a wall when they go too far in one direction

--- adventure.c
+++ adventure.c
@@ -8,14 +8,42 @@ int main(void) {
         {"bot_l","bot_m","bot_r"}
         };
     //move to other rooms from starting room[0][0]
+    //(x,y) notation flipped (y,x) for 2d arrays. First number is row(y) second is column(x)
     int yCoordinate = 0;
     int xCoordinate = 0;
-  
-    
-    scanf("%d", &xCoordinate);
-    scanf("%d", &yCoordinate);
 
-    // (x,y) notation flipped (y,x) for 2d arrays. First number is column(y) second is row (x)
     printf(room[yCoordinate][xCoordinate]);
 
- }
+    char direction = 'n';
+
+//movement system, quit game
+    while(direction != 'q'){
+    printf("\ngive direction: ");
+    scanf("\n %c", &direction);
+        if(direction == 's'){
+            yCoordinate = yCoordinate + 1;
+            printf(room[yCoordinate][xCoordinate]);
+        }
+        else if(direction == 'n'){
+            yCoordinate = yCoordinate - 1;
+            printf(room[yCoordinate][xCoordinate]);
+        }
+        //going too far to the 'e' direction has the array skip to the next set in line. so at room[0][2] + 'e' = room[1][0]. not sure why it does this. same in the opposite direction 'w'
+        else if(direction == 'e'){
+            xCoordinate = xCoordinate + 1;
+            printf(room[yCoordinate][xCoordinate]);
+        }
+        else if(direction == 'w'){
+            xCoordinate = xCoordinate - 1;
+            printf(room[yCoordinate][xCoordinate]);
+        }
+        else if(direction == 'q'){
+            printf("Goodbye!");
+        }
+        else{
+            printf("Please give one of the following: 'n','s','w','e'");
+        }
+    }
+    //need to figure out how to stop players if room[2][2] or room[0][0] to avoid seg fault. how do you set a max and min? another if statement seems messy
+
+}
