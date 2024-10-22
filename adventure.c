76f7d8fc7353b8271c030bfb2968c6927ade327d
BIIGORANGE <bigorangecreations@gmail.com>
2024-10-21 23:28:50 -0700

    added "walls" to game
    - made it so you can only move so far in the array solving 2 problems:
    -- you can no longer move to far in one direction
    --array will no longer start counting up on a different axis when the first runs out(ex. 'e' at room[0][2]) = room[1][0] for some reason)
    NEXT TODO:
    - create a character that can hold stats in it. this may be a function?

--- adventure.c
+++ adventure.c
@@ -7,43 +7,59 @@ int main(void) {
         {"mid_l","mid_m","mid_r"},
         {"bot_l","bot_m","bot_r"}
         };
-    //move to other rooms from starting room[0][0]
     //(x,y) notation flipped (y,x) for 2d arrays. First number is row(y) second is column(x)
     int yCoordinate = 0;
     int xCoordinate = 0;
 
     printf(room[yCoordinate][xCoordinate]);
 
-    char direction = 'n';
+    char direction;
 
-//movement system, quit game
+    //movement system, quit game
     while(direction != 'q'){
     printf("\ngive direction: ");
     scanf("\n %c", &direction);
-        if(direction == 's'){
+        //entering multiple of the same character will +1(-1) the array that many times. for example, "Give direction: ee" > xCoordinate + 2. or "Give direction: www" > xCoordinate -3.
+        if(direction == 'q'){
+            printf("Goodbye!");
+        }
+        else if(direction == 's' && yCoordinate < 2){
             yCoordinate = yCoordinate + 1;
             printf(room[yCoordinate][xCoordinate]);
         }
-        else if(direction == 'n'){
+        else if(direction == 'n' && yCoordinate > 0){
             yCoordinate = yCoordinate - 1;
             printf(room[yCoordinate][xCoordinate]);
         }
         //going too far to the 'e' direction has the array skip to the next set in line. so at room[0][2] + 'e' = room[1][0]. not sure why it does this. same in the opposite direction 'w'
-        else if(direction == 'e'){
+        else if(direction == 'e' && xCoordinate < 2){
             xCoordinate = xCoordinate + 1;
             printf(room[yCoordinate][xCoordinate]);
         }
-        else if(direction == 'w'){
+        else if(direction == 'w' && xCoordinate > 0){
             xCoordinate = xCoordinate - 1;
             printf(room[yCoordinate][xCoordinate]);
         }
-        else if(direction == 'q'){
-            printf("Goodbye!");
+        else if (direction == 's' && yCoordinate >= 2) {
+            printf("you can go no further\n");
+            printf(room[yCoordinate][xCoordinate]);
+        }
+        else if (direction == 'n' && yCoordinate <= 0) {
+            printf("you can go no further\n");
+            printf(room[yCoordinate][xCoordinate]);
+        }
+        else if (direction == 'e' && xCoordinate >= 2) {
+            printf("you can go no further\n");
+            printf(room[yCoordinate][xCoordinate]);
+        }
+        else if (direction == 'w' && xCoordinate <= 0) {
+            printf("you can go no further\n");
+            printf(room[yCoordinate][xCoordinate]);
         }
         else{
             printf("Please give one of the following: 'n','s','w','e'");
         }
     }
     //need to figure out how to stop players if room[2][2] or room[0][0] to avoid seg fault. how do you set a max and min? another if statement seems messy
-
+    return 0;
 }
